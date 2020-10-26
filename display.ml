d916870bb1408f4b4e570825b286dcbaa6086baf
Ben Bthd <39022534+BenBthd@users.noreply.github.com>
2020-10-26 23:42:29 +0100

    Correction commit
    Modules modeler refait et display complete

--- display.ml
+++ display.ml
@@ -3,10 +3,16 @@
 (*affiche une fenetre avec la grille d'origine*) 
 
 (* premiere version non graphique *)
+open String
+open Printf
 
-(*let display = fun crossword ->
-        for i = 0 to Array.lenght crossword -1 do
-                let line = "" in
-                for j = 0 to Array.lenght crossword.(0) -1 do
-line 
-*)
+let display = fun crossword ->
+        for i = 0 to (Array.length crossword) -1 do
+let line = ref "" in
+for j = 0 to (Array.length crossword.(0)) -1 do
+line := String.concat !line crossword.(i).(j);
+done;
+Printf.printf "%s\n" !line; 
+done
+
+
--- modeler.ml
+++ modeler.ml
@@ -3,5 +3,15 @@
 (*Transforme une grille sauvegardée en fichier.txt en un tableau de tableau *)
 (*Les donnees sont alors la valeur de la case avec 0 pour case vide, 1 pour case noire et la valeur de la lettre sinon *)
 
-(*let model_crossword = fun*) 
-
+let model_crossword = fun file_name ->
+try 
+let grid = open_in file_name in
+let dimensions = Scanf.sscanf (input_line grid) "%d %d" (fun n m -> [|n;m|]) in 
+let crossword = Array.make dimensions.(0) (Array.make dimensions.(1) 0) in
+let fill_element = fun grid_line nb_case element ->
+grid_line.[nb_case] in
+let fill_line = fun line -> 
+let grid_line = input_line grid in
+Array.mapi (fill_element grid_line) line in
+Array.map fill_line crossword
+with exc -> Printf.printf "%s ne s'ouvre pas en lecture\n" file_name; raise exc
