a43304de254c763314977c5eb2d764dfec2174f8
Ben Bthd <39022534+BenBthd@users.noreply.github.com>
2020-10-29 11:08:45 +0100

    Display/ Modeler/ Main/ Makefile/ references
    Corrections in Display and Modeler modules
    Adaptation to test mode of main module
    Creation of Makefile
    How to use the Makefile : link in references.txt

--- display.ml
+++ display.ml
@@ -8,11 +8,11 @@ open Printf
 
 let display = fun crossword ->
         for i = 0 to (Array.length crossword) -1 do             (*parcours des lignes *)
-        let line = ref "" in                                    (*texte ligne vierge *)
-        for j = 0 to (Array.length crossword.(0)) -1 do                 (*parcours termes de la ligne *)
-                line := String.concat !line crossword.(i).(j);          (*concatenation des termes - TODO Rajouter espace entre les termes*) 
-        done;
-        Printf.printf "%s\n" !line;             (* affichage ligne *)
-done
+                let line = ref "" in
+                for j = 0 to (Array.length crossword.(0) -1) do                         (*parcours des termes de la ligne *)
+                        line := String.concat " " [!line; Char.escaped crossword.(i).(j) ];          (*concatenation des termes de la liste [] avec le séparateur espace*) 
+                done;                                                                                   (*On a crossword un tableau de Char donc conversion en String pour concatener*)
+                Printf.printf "%s\n" !line;             (* affichage ligne *)
+        done
 
 
--- modeler.ml
+++ modeler.ml
@@ -6,11 +6,12 @@
 let model_crossword = fun file_name -> try 
         let grid = open_in file_name in                 (*ouverture du fichier avec gestion exceptions/erreurs *)
         let dimensions = Scanf.sscanf (input_line grid) "%d %d" (fun n m -> [|n;m|]) in         (*lecture de la premiere ligne avec les dimensions du tableau TODO possible de le déduire du reste *)
-        let crossword = Array.make dimensions.(0) (Array.make dimensions.(1) 0) in              (*creation tableau de tableau de 0 *)
+        let crossword = Array.make dimensions.(0) (Array.make dimensions.(1) '0') in              (*creation tableau de tableau de 0 *)
         let fill_element = fun grid_line nb_case element ->             (*fct interne remplissage des cases de chaque ligne *)
                 grid_line.[nb_case] in
         let fill_line = fun line ->                                     (* fct interne remplissage des lignes de la grille modelisee *)
                 let grid_line = input_line grid in
                 Array.mapi (fill_element grid_line) line in             (* application à chaque element (de la ligne) de la fct fill element *)
-        Array.map fill_line crossword                                   (* application à chaque ligne de la fct fill_line *)
+        Array.map fill_line crossword  (* application à chaque ligne de la fct fill_line *)
+        (*let () =close_in file_name *)                                         (* TODO probleme de fermeture du fichier, utiliser exception pour lire le fichier jusqu'a la fin puis fermeture*)
 with exc -> Printf.printf "%s ne s'ouvre pas en lecture\n" file_name; raise exc
