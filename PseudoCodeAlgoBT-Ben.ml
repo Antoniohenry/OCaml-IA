ee80fe538a68481d3208c5fe85e92c0157b20c61
Ben Bthd <39022534+BenBthd@users.noreply.github.com>
2020-11-13 18:57:28 +0100

    Algo - Pseudo Code Backtrack v1

--- PseudoCodeAlgoBT-Ben.ml
+++ PseudoCodeAlgoBT-Ben.ml
@@ -1,29 +1,52 @@
+(* Chargement des données nécessaires à l'algo *)
 
 (*On a une grille nommé Grille *)
-(* On appelle la fonction qui genere une liste de l'ensemble des mots de la grille *)
-(* mots = structure { id: int, coord_debut: int ,  coord_fin: int, liste de l'index des mots croisés par ce mot: int list }  *)
-(* on appelle ensuite une fonction qui cherche les voisins de chaque mot et rajoute leurs id dans chaque liste correspondante *)
+(* On genere une liste de l'ensemble des mots de la grille (structure word) et qu'on note words *)
+(* word = structure { id: int, coord_debut: int*int ,  coord_fin: int*int, liste de l'index des word croisés par ce mot: int list }  *)
+(* on appelle ensuite une fonction qui cherche les voisins de chaque word dans la grille et rajoute leurs id dans chaque liste correspondante *)
 
-(* on a une fonction create_dico(word) qui genere un dictionnaire de mots compatibles avec le word donné (taille et caracteres presents) *)
+(* on a une fonction create_dico(words) qui genere un dictionnaire de mots compatibles avec le premier word de la liste words donnée (taille et caracteres presents) *)
+(* ou une liste vide si words est vide *)
+
 
 (*1er appel de la fonction *)
 
-algo(Grille,words,create_dico(words[0]))  (*on prend une grille initiale, liste de mots, dictionnaire, mot choisi *)
+algo(Grille,words,create_dico(words))  (*on prend une grille initiale, liste de mots à visiter, dictionnaire associé au premier mot de words si il existe *)
 
 
-(*PseudoCode *)
+(*Algo*)
 
-(* On choisit un noeud initial avec une grille initiale et liste de mots à visiter *)
-(* on applique l'algo : *)
-(* Si il n'y a pas de mot à visiter dans la liste c'est a dire pas de nouveaux noeuds, on a atteint une feuille de mon arbre, la grille est remplie *)
-(* Sinon, on prend le premier mot à visiter de la liste : on voit donc un nouveau noeud, et on verifie si il y a des branches partant de ce mot *)
-(* Les branches sont ici données par les mots du dico compatibles avec ce mot/noeud à visiter *)
-(* Si il n'y a pas de mots compatibles, on ne va pas visiter le en renvoyant False et on considère le *)
-(* On prend le premier terme de la liste des mots compatibles  *)
+(* Si la liste de noeuds/words est vide, il n'y a pas de noeud/word à visiter, alors on a atteint une feuille de l'arbre, la grille est remplie *)
+  (* On prévient que c'est bon : True *)
+
+(* Sinon, on prend le premier word à visiter de la liste : on voit donc un nouveau noeud, et on verifie si il y a des branches partant de ce mot *)
+(* Les branches sont ici données par les termes du dico compatibles avec ce word/noeud à visiter *)
+
+  (* Si il n'y a pas de termes compatibles, ce noeud n'a pas de branches donc pas de feuilles *)
+  (* On prévient que ce n'est pas un bon noeud : False *)
+
+  (* Sinon, il y a au moins un terme compatible/ une branche, on fixe le premier terme compatible de la liste dans la grille c'est à dire qu'on prend cette branche *)
+  (* on considère qu'on a visité le noeud précédent et donc on enleve le noeud/word de la liste des noeuds/words à visiter *)
+
+  (* On vérifie alors si en partant dans cette branche, on peut atteindre une feuille :
+  (* on applique le meme algorithme en donnant en paramètre la grille avec notre branche choisie dedans et la nouvelle liste des noeuds/words à visiter (réorganisée) *)
+  (* Et le dictionnaire associé à cette nouvelle liste de noeuds *)
+  
+  (* si l'algorithme trouve bien que cette branche donne accès à une feuille : grille remplie (True) on renvoie une reponse positive True *)
+  (* Sinon, cette branche ne mène à aucune feuille on revient au noeud précédent : réinitialise la grille et la liste des mots *)
+    
+    (* Si il n'y a pas d'autre feuilles partant de ce noeud (termes compatibles avec ce noeud/word *)
+      (* On renvoie que ce noeud ne mène à aucune feuille : False *)
+
+    (* Si il y a encore une autre feuille partant de ce noeud (donc qu'il y avait au moins deux termes compatibles au depart avec le noeud *)
+      (* On réapplique l'algorithme avec l'ancienne grille, la liste de mot à visiter (réinitialisés) et le dictionnaire auquel on a enlevé la dernière branche *)
+      (* et on renvoie le résultat pour savoir si en revenant au noeud puis en prenant une nouvelle branche, on atteint une feuille *)
 
 
 
 
+
+(*Pseudo Code *)
 
 
 algo(Grille, words, dico):        (* Noeud : { liste words de word (mot de la grille) + grille } , mots compatibles du dico : branches partant du noeud *)
