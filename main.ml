cde69b27e8c7335409adc9f4078256fc250a161d
Antoniohenry <58167541+antoniohenry@users.noreply.github.com>
2020-11-23 23:47:49 +0100

    ajout d'une partie des fonctions d'analyse de la grille changement d'exemple mise a jour du README.md
    Signed-off-by: etudiant <etudiant>

--- main.ml
+++ main.ml
@@ -16,17 +16,18 @@ Generator.generate_crossword nom_grille  (*utile pour plus tard lors de series d
 
 (*Chargement de la grille sauvegardé avec la fct "model_crossword" du module modeler avec param  "fichier.txt" pour la modeliser en tableau pouvant être analysé *)  
 let nom_grille = "exemple.txt" in
-(* let crossword = Modeler.model_crossword nom_grille in *)
-let structure = Modeler.model_crossword nom_grille in
-let table_grid = Modeler.get_table structure in
+let table_grid = Modeler.get_table (Modeler.model_crossword nom_grille) in
 
 
 (*Analyse de la grille modelisee precedemment avec la fct "analyse_crossword" du module analyser avec param le tableau grille *)
 (*qui renvoie la meme grille si elle n'est pas valide ou la grille remplie sinon *)
+let words = Modeler.get_words table_grid in
+Modeler.print_words words; Display.display table_grid ;;
 
 (*let crossword = Analyser.analyse_crossword grid *)
 
 
 (*Affichage du succès de la grille remplie si elle est valide ou de l'originale sinon *)
-Display.display table_grid
+
+
 
