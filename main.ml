94f9b2feafad5e28a39463273bb0317316b2d70b
Ben Bthd <39022534+BenBthd@users.noreply.github.com>
2021-01-11 23:13:56 +0100

    Update main.ml

--- main.ml
+++ main.ml
@@ -4,12 +4,16 @@
 open Status
 
 let () = 
+let all = ref False in
+let nom_grille = if (Array.length Sys.argv)=1 then begin
+    if (Sys.argv.(1) = "all") then  all := True ; "exemple.txt" else Sys.argv.(1) end
+else all := True ; Sys.argv.(1) in
 
-let nom_grille = if (Array.length Sys.argv)=1 then "exemple.txt" else Sys.argv.(1) in 
-let dico_name = "dico_fr.txt" in  
+let dico_name = "dico_fr.txt" in
+
 try
 let status = Reader.read nom_grille dico_name in
-if Bt.bt status then failwith "il n'y a pas de solution à cette grille"
+if Bt.bt status all then failwith "il n'y a pas de solution à cette grille"
 with e -> raise e;;
 
 
